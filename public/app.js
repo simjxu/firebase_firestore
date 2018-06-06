ef02a90e417198cdf15d08b1793ad2048614f01e
Simon Xu <simon@petasense.com>
2018-06-05 22:10:07 -0700

    Minor edits

--- public/app.js
+++ public/app.js
@@ -6,12 +6,12 @@ console.log("test")
 // Function for what the click button does
 function submitClick() {
 	var firebaseRef = firebase.database().ref();
-	firebaseRef.child("Something").set("Some Value");
+	firebaseRef.child("mote").set("New Value");
 };
 
 // Reference HTML section for an object before it was made, and then the database object
-const preObject = document.getElementById("object");
-const dbRefObject = firebase.database().ref().child("object");
+const preObject = document.getElementById("mote");
+const dbRefObject = firebase.database().ref().child("mote");
 
 // Show in the console whenever a value changes in the realtime database
 dbRefObject.on("value", snap => console.log(snap.val()));
